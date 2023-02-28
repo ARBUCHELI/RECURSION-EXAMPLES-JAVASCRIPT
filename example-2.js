e0d74249168e232a3c3fc5c2d1be3fd8f3572cdd
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-28 13:11:10 -0500

    creating example 2

---
+++ example-2.js
@@ -0,0 +1,8 @@
+//Factorial
+function findFactorial(num) {
+    if (num === 0) return 1
+    let factorial = num * findFactorial(num - 1)
+    return factorial;
+}
+  
+console.log(findFactorial(5));
